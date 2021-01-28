b7f99893c34183501cff019c28f33431e8519d86
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-01-28 16:10:00 +0100

    makin print better

--- task2_commands.py
+++ task2_commands.py
@@ -8,7 +8,7 @@ no_ways = 1
 
 cache_line_counter = 0
 block_size_counter = 0
-example_list = ['fibbonaci', 'matmul', 'memcopy', 'pi', 'whetstone']
+example_list = ['fibbonaci', 'matmul', 'memcopy', 'whetstone']
 for block_size_counter in range(0, 5):
     for exmaple_name in example_list:
         p1 = subprocess.run(
@@ -16,7 +16,7 @@ for block_size_counter in range(0, 5):
         x1 = p1.stderr.decode().find('dl1.miss_rate ')
         x2 = p1.stderr.decode().find('ul2.miss_rate ')
         print(exmaple_name, 'no_ways,', no_ways, 'nsets', nsets,
-              ' dl1miss_rate ', p1.stderr.decode()[x1:x1+35], 'ul2miss_rate', p1.stderr.decode()[x2:x2+35], '\n')
+              ' dl1miss_rate ', p1.stderr.decode()[x1+29:x1+35], 'ul2miss_rate ', p1.stderr.decode()[x2+29:x2+35], '\n')
     no_ways = no_ways*2
     nsets = int(nsets/2)
     print('\n \n \n \n')
