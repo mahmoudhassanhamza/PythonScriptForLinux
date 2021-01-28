339d16959e59cbaef930b477f87eb2d026be3173
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-01-28 16:17:10 +0100

    makin print better

--- task2_commands.py
+++ task2_commands.py
@@ -9,9 +9,8 @@ no_ways = 1
 cache_line_counter = 0
 block_size_counter = 0
 example_list = ['fibbonaci', 'matmul', 'memcopy', 'whetstone']
-for exmaple_name in example_list:
-	for block_size_counter in range(0, 5):
-
+for block_size_counter in range(0, 5):
+    for exmaple_name in example_list:
         p1 = subprocess.run(
             f'./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:{nsets}:32:{no_ways}:l -cache:il2 dl2 -cache:dl2 ul2:1024:64:2:l ./benchmarks/{exmaple_name}', shell=True, stderr=subprocess.PIPE)
         x1 = p1.stderr.decode().find('dl1.miss_rate ')
