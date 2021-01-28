883a949ab84fa4ff2b6f2fe943ba81a6a00817ba
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-01-28 15:56:27 +0100

    adjusting print to look better

--- task2_commands.py
+++ task2_commands.py
@@ -16,8 +16,9 @@ for block_size_counter in range(0, 5):
         x1 = p1.stderr.decode().find('dl1.miss_rate ')
         x2 = p1.stderr.decode().find('ul2.miss_rate ')
         print(exmaple_name, 'no_ways,', no_ways, 'nsets', nsets,
-              ' dl1miss_rate ', p1.stderr.decode()[x1:x1+35], 'ul2miss_rate', p1.stderr.decode()[x2:x2+35])
+              ' dl1miss_rate ', p1.stderr.decode()[x1:x1+35], 'ul2miss_rate', p1.stderr.decode()[x2:x2+35], '\n')
     no_ways = no_ways*2
     nsets = int(nsets/2)
+    print('\n \n \n \n')
 
     # ./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:256:32:1:l -cache:il2 dl2 -cache:dl2 ul2:{nsets}:32:{no_ways}:l ./benchmarks/fibonacci
