e4789e3dee6a34b34c6fcdb57ef32a94eb757233
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-01-28 15:52:01 +0100

    adding example names

--- task2_commands.py
+++ task2_commands.py
@@ -1,4 +1,5 @@
-import  subprocess 
+import subprocess
+
 
 cache_line = 1024
 block_size = 8
@@ -7,17 +8,16 @@ no_ways = 1
 
 cache_line_counter = 0
 block_size_counter = 0
+example_list = ['fibbonaci', 'matmul', 'memcopy', 'pi', 'whetstone']
+for block_size_counter in range(0, 5):
+    for exmaple_name in example_list:
+        p1 = subprocess.run(
+            f'./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:{nsets}:32:{no_ways}:l -cache:il2 dl2 -cache:dl2 ul2:1024:64:2:l ./benchmarks/{exmaple_name}', shell=True, stderr=subprocess.PIPE)
+        x1 = p1.stderr.decode().find('dl1.miss_rate ')
+        x2 = p1.stderr.decode().find('ul2.miss_rate ')
+        print(exmaple_name, 'no_ways,', no_ways, 'nsets', nsets,
+              ' dl1miss_rate ', p1.stderr.decode()[x1:x1+35], 'ul2miss_rate', p1.stderr.decode()[x2:x2+35])
+    no_ways = no_ways*2
+    nsets = int(nsets/2)
 
-for block_size_counter in range (0,5):
-	p1 = subprocess.run(f'./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:{nsets}:32:{no_ways}:l -cache:il2 dl2 -cache:dl2 ul2:1024:64:2:l ./benchmarks/whetstone',shell  =True, stderr = subprocess.PIPE)
-	x= p1.stderr.decode().find('dl1.miss_rate ')
-
-	print('no_ways',no_ways,'  nsets',nsets,' miss_rate ',p1.stderr.decode()[x:x+35])
-	no_ways =no_ways*2
-	nsets = int(nsets/2)
-
-
-
-	#./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:256:32:1:l -cache:il2 dl2 -cache:dl2 ul2:{nsets}:32:{no_ways}:l ./benchmarks/fibonacci
-
-
+    # ./sim-cache -cache:il1 il1:128:64:1:l -cache:dl1 dl1:256:32:1:l -cache:il2 dl2 -cache:dl2 ul2:{nsets}:32:{no_ways}:l ./benchmarks/fibonacci
---
+++ test.py
@@ -0,0 +1,8 @@
+import csv
+with open('results.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([1, "Hamza", "Mahmoud"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
